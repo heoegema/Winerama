0ebaecd801ec7b0deb7c2523c833b706e84f5833
Holly Oegema <heoegema@Hollys-MacBook-Air.local>
2017-02-01 22:00:26 -0500

    Simple Wine review site where the user can create an account and write reviews and such

---
+++ reviews/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    # ex: /
+    url(r'^$', views.review_list, name='review_list'),
+    # ex: /review/5/
+    url(r'^review/(?P<review_id>[0-9]+)/$', views.review_detail, name='review_detail'),
+    # ex: /wine/
+    url(r'^wine$', views.wine_list, name='wine_list'),
+    # ex: /wine/5/
+    url(r'^wine/(?P<wine_id>[0-9]+)/$', views.wine_detail, name='wine_detail'),
+    url(r'^wine/(?P<wine_id>[0-9]+)/add_review/$', views.add_review, name='add_review'),
+    # ex: /review/user - get reviews for the logged user
+    url(r'^review/user/(?P<username>\w+)/$', views.user_review_list, name='user_review_list'),
+    url(r'^review/user/$', views.user_review_list, name='user_review_list'),
+]
